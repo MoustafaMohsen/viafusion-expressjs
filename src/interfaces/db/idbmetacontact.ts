f0fd27e221fb15e82fdd0d17c8567b3542a4b339
Moustafa Mohsen <moustafamohsen1@gmail.com>
2021-07-03 23:27:26 +0200

    meta service init

---
+++ src/interfaces/db/idbmetacontact.ts
@@ -0,0 +1,37 @@
+import { ISender } from '../rapyd/isender';
+import { PostCreatePayment } from '../rapyd/ipayment';
+import { customert_id } from '../rapyd/types';
+import { ewallet_id } from "../rapyd/types";
+import { IPayment } from '../rapyd/ipayment';
+/**
+ *
+contact_reference_id VARCHAR ( 255 ) PRIMARY KEY,
+id VARCHAR ( 255 ),
+phone_number VARCHAR ( 255 ),
+email VARCHAR ( 255 ),
+wallet_id VARCHAR ( 255 ),
+wallet_refrence_id VARCHAR ( 255 ),
+data TEXT
+
+ */
+
+export interface IDBMetaContact {
+  /** Internal id for calling actions */
+  id?: string;
+  contact_reference_id?: ewallet_id;
+  wallet_refrence_id?: string;
+  customer?: customert_id;
+  meta?: object;
+  transactions?: ITransaction[];
+  senders?: ISender[];
+  bene?: any[];
+}
+
+export interface ITransaction {
+  id: string
+  source_amount?: string
+  destination_amount?: string
+  sources: PostCreatePayment.ICreate[]
+  destinations: any[]
+  executed: boolean
+}
