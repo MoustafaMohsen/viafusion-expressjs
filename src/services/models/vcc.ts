49b6ba2f0f473f94e6a84df31a42a07db08a32d2
Moustafa Mohsen <moustafamohsen1@gmail.com>
2021-07-04 04:15:40 +0200

    vcc

--- src/services/models/vcc.ts
+++ src/services/models/vcc.ts
@@ -18,36 +18,49 @@ export class VccService {
         return apiSrv.post<IssueVccResponse>("issuing/cards", card_info)
     }
 
-    async create_vcc_step(form: IssueVccRequestForm, contact_reference_id: number):Promise<IDBContact> {
+    activate_card(card_number: string) {
+        var apiSrv = new ApiService();
+        return apiSrv.post<IssueVccResponse>("issuing/cards/activate", {
+            card: card_number
+        })
+    }
+
+    async create_vcc_step(form: IssueVccRequestForm, contact_reference_id: number): Promise<IDBContact> {
         let userSrv = new UserService();
         let user = await userSrv.get_db_user({ contact_reference_id });
 
         let update_user: IssueVccRequestForm = {
-            date_of_birth:form.date_of_birth,
-            address:form.address,
-            country:form.country
+            date_of_birth: form.date_of_birth,
+            address: form.address,
+            country: form.country
         }
-        
+
         return new Promise((resolve, reject) => {
             let walletSrv = new WalletService();
-            walletSrv.update_contact(user.ewallet,user.contact,update_user as any).then(async (res) => {
+            walletSrv.update_contact(user.ewallet, user.contact, update_user as any).then(async (res) => {
                 let contactdata = res.body.data;
                 user.rapyd_contact_data = contactdata;
                 user.contact = contactdata.id;
                 user = await userSrv.update_db_user({ contact_reference_id: user.contact_reference_id }, user);
                 this.create_vcc({
-                    country:form.country,
-                    ewallet_contact:user.contact
+                    country: form.country,
+                    ewallet_contact: user.contact
                 }).then(async (card) => {
 
-                    // update cards in metacontact
-                    let metacontactSrv = new MetaContactService();
-                    let metacontact = await metacontactSrv.get_db_metacontact({contact_reference_id:user.contact_reference_id} as any);
-                    metacontact.vcc.push(card.body.data);
-                    metacontact = await metacontactSrv.update_db_metacontact({contact_reference_id:user.contact_reference_id} as any, metacontact);
-                    // return contact
-                    resolve(user);
 
+                    let card_data = card.body.data;
+                    this.activate_card(card_data.card_number).then(async (card_data) => {
+                        // update cards in metacontact
+                        let metacontactSrv = new MetaContactService();
+                        let metacontact = await metacontactSrv.get_db_metacontact({ contact_reference_id: user.contact_reference_id } as any);
+                        metacontact.vcc.push(card_data);
+                        metacontact = await metacontactSrv.update_db_metacontact({ contact_reference_id: user.contact_reference_id } as any, metacontact);
+                        // return contact
+                        resolve(user);
+                    }).catch(error => {
+                        console.error(error);
+                        reject(error.status.message);
+                    })
                 }).catch(error => {
                     console.error(error);
                     reject(error.status.message);
